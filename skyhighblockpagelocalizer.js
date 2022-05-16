190ed84f5d84a8f138b88adbc043a5789aa6fe36
andreas.zengel@gmail.com <andreas.zengel@gmail.com>
2022-05-16 19:58:29 +0200

    Create skyhighblockpagelocalizer.js

---
+++ skyhighblockpagelocalizer.js
@@ -0,0 +1,12 @@
+// skyhighblockpagelocalizer
+
+function helloWorld() {
+  console.info("Hello World");
+}
+
+function localize(inObj) {
+  console.debug("starting localize");
+  console.debug(inObj);
+  console.debug("done with localize");
+}
+  
